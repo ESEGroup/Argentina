9ff5d9b50f0fd6850f65cffc61e88069c6d48740
Luis <luisfnicolau@hotmail.com>
2016-12-19 23:02:57 -0200

    Adicionada as classes Aluno e professor recrutador ao painel de admin

--- ofertas/admin.py
+++ ofertas/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Oferta, Candidato
+from .models import Oferta, Candidato, Aluno, ProfessorRecrutador
 
 admin.site.register(Oferta)
 admin.site.register(Candidato)
+admin.site.register(Aluno)
+admin.site.register(ProfessorRecrutador)
